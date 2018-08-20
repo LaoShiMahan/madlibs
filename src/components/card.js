557ebe3f9b77d610b8b01971370391e2a0d74d6e
Brodie Kelley <brodiekelley@hotmail.com>
2018-08-20 14:47:25 -0600

    Set state and initial input value in card.js

--- src/components/card.js
+++ src/components/card.js
@@ -3,9 +3,24 @@ import React, { Component } from 'react';
 import Input from './input';
 
 class Card extends Component {
+    constructor() {
+        super()
+        this.state = {
+            color: ''
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+    handleInputChange(event) {
+        this.setState({ color: event.target.value })
+    }
+
     render() {
         return (
             <div className="card">
+                <h1>{this.state.color}</h1>
+                <input value={this.state.color} onChange={(event) => this.handleInputChange(event)}/>
                 { Input('Color') }
             </div>
         )
